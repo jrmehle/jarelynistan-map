07fdd0c934b7f305f4effa76750ef959c416595e
Jared Mehle <jrmehle@gmail.com>
2018-01-27 13:51:45 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1517082569,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "Jarelynistan",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "myworld_isometric_t1",
+      "tileSize": 384,
+      "world": "myworld",
+      "worldName": "myworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "myworld_isometric_t1": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
